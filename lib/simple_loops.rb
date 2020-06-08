caff98bc28f22f5560b36b4aae33e9bcfd10b20b
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-08 20:22:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,8 @@
 # Write your methods here
 def loop_message_five_times(message)
   count = 0
-  message = "Hello World."
   while count < 5 do
     puts message
-    count +=
+    count += 1
   end
+end
